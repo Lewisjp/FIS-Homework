a25c0b934060fbbeecfd3e65e11f349b8c329697
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-11-14 22:02:47 -0500

    Day 4 HW

--- number-guesser/number-guesser.rb
+++ number-guesser/number-guesser.rb
@@ -1,39 +1,93 @@
+	# tell the user if they are getter warmer or colder depending on if their newest guess is closer or further from the answer than their last guess
+
+def answer_temp(guess, answer)
+	temp = (guess - answer).abs	
+	if temp < 10
+		puts "You're so close you're on fire."
+	else
+		if temp < 20
+			puts "You're hot."
+		else
+			if temp < 30 
+				puts "You're getting warmer."
+			else
+				if temp < 50
+					puts "It feels too warm to wear a jacker, but too cold to wear a t-shirt."
+				else
+					if temp < 80
+						puts "Is it just me, or are you cold?"
+					else
+						if temp < 100
+							puts "You must be freezing."
+						end
+					end
+				end
+			end
+		end
+	end
+end 
 
 	# Tell the user if they were correct or incorrect
 
-def prompt(guess, answer)
+def prompt(guess, answer, tried_again)
 
 	if guess == answer
 		puts "Congratulations!  You chose wisely" 
 		return true
 	else
-		puts "You chose #{guess}, the answer was #{answer}.  Maybe next time." 
-		return false
+
+		# tell the user if their guess  are 'higher' or 'lower' than the correct number
+
+		if guess > answer 
+			puts "You chose too high with #{guess}."
+			if tried_again == true 
+				answer_temp(guess, answer)
+			end
+		else
+			puts "You chose too low with #{guess}."
+			if tried_again == true 
+				answer_temp(guess, answer)
+			end
+		end
+
+		# give them the option to play the game again
+
+		puts "Would you like to try again? Yes or No?"
+			tried_again = true
+			answer_2 = gets.chomp.downcase
+			case answer_2
+				when "yes" then input(answer, tried_again)
+				when "y" then input(answer, tried_again)
+				else
+					return false
+			end
 	end
 
 end
 
+
 	# Should ask the user to enter a number between 1 and 100
 	# receive input from the user
 
-def input
+def input(answer, tried_again)
 
-	answer = rand(99) + 1
-
-	puts "\n\nGuess a number between 1 and 100 correctly."
+	puts "\nGuess a number between 1 and 100 correctly."
 	guess = gets.chomp.to_i
 
 	if guess < 101 && guess > 0 
-		prompt(guess, answer)
+		prompt(guess, answer, tried_again)
 	else
 		puts "The cowboy with wise old eyes sighs.. you lost your chance for free admission." 
 		return false
 	end
 
+
 end
 
-puts "While camping In the velvet darkness of the blackest night, you suddenly see a guiding light.  After walking along the river you begin to hear music, and once over the hill see the carnival.  At the entrance stands an older man in a cowboy hat.\nHe looks up from the ground to meet your eyes...\nAdmission is free if you..."
+tried_again = false 
+answer = rand(99) + 1
+puts "While camping in the velvet darkness of the blackest night, you suddenly see a guiding light.  After walking along the river you begin to hear music, and once over the hill see the carnival.  At the entrance stands an older man in a cowboy hat.\nHe looks up from the ground to meet your eyes...\nAdmission is free if you..."
 
-input()
+input(answer, tried_again)
 
 
--- number-guesser/number-guesser_spec.rb
+++ number-guesser/number-guesser_spec.rb
@@ -3,30 +3,38 @@ require_relative './number-guesser'
 describe "#input" do
   it "returns false if an guess was out of range of 1 - 100" do
   	guess = 200
-    input.should eq(false)
+    tried_again = false
+    answer = 50 
+    input(answer, tried_again).should eq(false)
   end
 
   it "returns false if an guess was out of range of 1 - 100" do
   	guess = -1
-    input.should eq(false)
+    tried_again = false
+    answer = 50 
+    input(answer, tried_again).should eq(false)
   end
 
   it "returns false if an guess was out of range of 1 - 100" do
   	guess = 0
-    input.should eq(false)
+    tried_again = false
+    answer = 50 
+    input(answer, tried_again).should eq(false)
   end
 
 
   it "returns true if an valid guess was given" do
   	answer = 5
   	guess = answer
-    prompt(guess, answer).should eq(true)
+    tried_again = false
+    prompt(guess, answer, tried_again).should eq(true)
   end
 
   it "returns false if an invalid guess was given" do
   	answer = 5
   	guess = 89
-    prompt(guess, answer).should eq(false)
+    tried_again = false
+    prompt(guess, answer, tried_again).should eq(false)
   end
   
 end
