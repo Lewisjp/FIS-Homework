2cca2284bc70f3fcdc782461203764b414ef0335
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-11-20 08:44:38 -0500

    ref fixed, added another test.

--- day7/remove_nils_solution_spec.rb
+++ day7/remove_nils_solution_spec.rb
@@ -1,4 +1,4 @@
-requite './remove_nils_solution'
+require './remove_nils_solution'
 # prereqs arrays, methods, conditional logic, chaining
 # Stop this code from throwing an exception. 
 
@@ -7,12 +7,21 @@ describe "#some_method" do
 	it "returns the input array squared" do
 		expect(some_method).to be_a_kind_of(Numeric)
     end
-	
+
+    it "should return an array in which new elements are the square of the original elements" do 
+        expect{some_method}.to_not raise_exception
+    end
+
 end
 
 =begin
 def some_method
   [1, 4, nil, 9, 16, nil].compact.inject(0) {|sum, number| sum + number}
 end
+
+
+
+
+
 =end
 
